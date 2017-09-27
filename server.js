d0e8588453badd0d1558c64dad25664c53d5ece3
monikakindernayova <monika.kindernayova@skyscanner.net>
2017-09-27 23:55:46 +0100

    Add mongo db

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ connections = [];
 Schema = new mongoose.Schema({
     message : String
 });
-
-//const Message = mongoose.model('messages', Schema);
+const Message = mongoose.model('messages', Schema);
 const MONGOLAB_URI = "mongodb://heroku_z45zn901:alrmhkl5eg87ao5drig0doctb6@ds139989.mlab.com:39989/heroku_z45zn901";
 
 mongoose.connect(MONGOLAB_URI, function (error) {
@@ -25,27 +24,26 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket){
-    //connections.push(socket);
-    //console.log('connected %s sockets connected', connections.length)
-    //
-    //socket.on('disconnect', function(socker){
-    //    connections.splice(connections.indexOf(socket), 1);
-    //    console.log('Disconnected %s sockets connected', connections.length)
-    //});
-    //
-    //socket.on('send message', function(data){
-    //    const newMessage = new Message({message: data});
-    //        newMessage.save(function (err) {
-    //    });
-    //    io.sockets.emit('add messages', [{message: data}]);
-    //});
-    //
-    //socket.on('client start', function() {
-    //    Message.find(function (err, messages) {
-    //        io.sockets.emit('add messages', messages);
-    //    });
-    //});
+    connections.push(socket);
+    console.log('connected %s sockets connected', connections.length)
 
+    socket.on('disconnect', function(socker){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected %s sockets connected', connections.length)
+    });
+
+    socket.on('send message', function(data){
+        const newMessage = new Message({message: data});
+            newMessage.save(function (err) {
+        });
+        io.sockets.emit('add messages', [{message: data}]);
+    });
+
+    socket.on('client start', function() {
+        Message.find(function (err, messages) {
+            io.sockets.emit('add messages', messages);
+        });
+    });
 });
 
 
