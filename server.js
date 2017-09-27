73a4841b12f83f684d8c90188a5ebe908dd3af7b
monikakindernayova <monika.kindernayova@skyscanner.net>
2017-09-27 23:39:00 +0100

    Add mongo db

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ Schema = new mongoose.Schema({
     message : String
 });
 
-const Message = mongoose.model('messages', Schema);
-//const MONGOLAB_URI = "mongodb://heroku_z45zn901:alrmhkl5eg87ao5drig0doctb6@ds139989.mlab.com:39989/heroku_z45zn901";
+//const Message = mongoose.model('messages', Schema);
+const MONGOLAB_URI = "mongodb://heroku_z45zn901:alrmhkl5eg87ao5drig0doctb6@ds139989.mlab.com:39989/heroku_z45zn901";
 
-//mongoose.connect(MONGOLAB_URI, function (error) {
-//    if (error) console.error(error);
-//    else console.log('mongo connected');
-//});
+mongoose.connect(MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
 
 server.listen(process.env.PORT || 4000);
 console.log('server running...');
